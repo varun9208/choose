96d24028bc83c3fbbc4e96cafc9d584ee94db51c
Varun <Varun@Varun-Guptas-MacBook-Pro.local>
2013-03-19 01:45:20 +0530

    Initial Commit

---
+++ choose/main.c
@@ -0,0 +1,18 @@
+//
+//  main.c
+//  choose
+//
+//  Created by Varun on 3/19/13.
+//  Copyright (c) 2013 Varun. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[])
+{
+
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
+
